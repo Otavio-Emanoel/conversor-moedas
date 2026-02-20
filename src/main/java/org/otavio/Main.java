6579ef166c1f16b2631b17f1a59507dee2d06b71
Otavio-Emanoel <otavioemanuel6b@gmail.com>
2026-02-20 18:53:27 -0300

    adiciona Scanner e instancia para receber o valor em double

--- src/main/java/org/otavio/Main.java
+++ src/main/java/org/otavio/Main.java
@@ -4,8 +4,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double valorReais = 100;
-        
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o valor que deseja converter: ");
+        double valorReais = scanner.nextDouble();
         double realParaDolar = 0.19;
         double valorRealEmDolar = valorReais * realParaDolar;
         System.out.println(valorRealEmDolar);
